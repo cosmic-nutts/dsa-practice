9e9835a993e99ae965604fba1fce712f4c6889b4
Natasha-Yadav <natasha100913@gmail.com>
2025-06-23 22:00:12 +0530

    dijkstra algo

--- graph/practice.cpp
+++ graph/practice.cpp
@@ -1,31 +1,64 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
 
+vector<int> dijkstra(int V, vector<vector<int>> adj[], int S) {
+    // Priority queue (min heap)
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    
+    vector<int> dis(V);
+    for(int i = 0; i < V; i++) {
+        dis[i] = 1e9;
+    }
+    dis[S] = 0;
+    pq.push({0, S});
 
+    while(!pq.empty()) {
+        int distance = pq.top().first;
+        int node = pq.top().second;
+        pq.pop();
 
-vector<int> bfs(int v;int node;vector<int>adj){
-    queue<int>q;
-    int vis[v]={0};
-    vector<int>ans;
-
-    vis[0]=1;
-    q.push(0);
-
-    while(!q.empty()){
-        int node=q.front();
-        q.pop();
-        for(auto it: adj[node]){
-            if(!vis[it]){
-                vis[it]=1;8
-                q.push(it);
+        for(auto it : adj[node]) {
+            int adjnode = it[0];
+            int edgewt = it[1];
+            if(distance + edgewt < dis[adjnode]) {
+                dis[adjnode] = distance + edgewt;
+                pq.push({dis[adjnode], adjnode});
             }
         }
     }
-
-
+    return dis;
 }
 
+// Driver Code
 int main() {
+    int V = 5; // Number of vertices in the graph
+    vector<vector<int>> adj[V];
+    
+    // Adding edges to the graph
+    // Format: {adjacent node, weight}
+    adj[0].push_back({1, 2});
+    adj[0].push_back({3, 1});
+    adj[1].push_back({0, 2});
+    adj[1].push_back({2, 4});
+    adj[1].push_back({4, 5});
+    adj[2].push_back({1, 4});
+    adj[2].push_back({3, 3});
+    adj[3].push_back({0, 1});
+    adj[3].push_back({2, 3});
+    adj[3].push_back({4, 1});
+    adj[4].push_back({1, 5});
+    adj[4].push_back({3, 1});
+    
+    int S = 0; // Source vertex
+    
+    vector<int> res = dijkstra(V, adj, S);
+    
+    cout << "Shortest distances from source " << S << ":\n";
+    for(int i = 0; i < V; i++) {
+        cout << "Vertex " << i << ": " << res[i] << endl;
+    }
     
     return 0;
 }
