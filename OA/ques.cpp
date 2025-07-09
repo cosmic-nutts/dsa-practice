db3e7ab499a19b9d41378401ffe183e55c17d724
Natasha-Yadav <natasha100913@gmail.com>
2025-07-09 23:17:04 +0530

    easy ques

---
+++ OA/ques.cpp
@@ -0,0 +1,16 @@
+factorial
+
+int #include <iostream>
+using namespace std;
+
+int fact(int n){
+    if(n == 0 || n == 1) {
+        return 1;
+    }
+    return n * fact(n - 1);
+}
+
+int main() {
+    
+    return 0;
+}
