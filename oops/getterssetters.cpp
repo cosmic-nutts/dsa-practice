e2409710a749be4b386f22f76bbfc6b66f7498f8
Natasha-Yadav <natasha100913@gmail.com>
2025-07-13 20:26:42 +0530

    oops setters and getters

--- oops/getterssetters.cpp
+++ oops/getterssetters.cpp
@@ -2,17 +2,37 @@
 using namespace std;
 
 class Person {
-public:
     string name;
     int age;
+public:
+    // Getter for name
+    string getName() const {
+        return name;
+    }
+    // Setter for name
+    void setName(const string& newName) {
+        name = newName;
+    }
+    // Getter for age
+    int getAge() const {
+        return age;
+    }
+    // Setter for age
+    void setAge(int newAge) {
+        if (newAge >= 0) {
+            age = newAge;
+        } else {
+            cout << "Age cannot be negative." << endl;
+        }
+    }
 };
 
 int main() {
     Person p1;
-    p1.name = "John Doe";
-    p1.age = 30;
+    p1.setName("John Doe");
+    p1.setAge(30);
 
-    cout << "Name: " << p1.name << endl;
-    cout << "Age: " << p1.age << endl;
+    cout << "Name: " << p1.getName() << endl;
+    cout << "Age: " << p1.getAge() << endl;
     return 0;
 }
