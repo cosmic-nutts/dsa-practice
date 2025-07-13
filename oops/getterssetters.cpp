fb4bea080b3d82f4a5c1294c5b10af76aa7d0c5d
Natasha-Yadav <natasha100913@gmail.com>
2025-07-13 20:20:17 +0530

    oops public

--- oops/getterssetters.cpp
+++ oops/getterssetters.cpp
@@ -1,17 +1,18 @@
-int #include <iostream>
+#include <iostream>
 using namespace std;
 
 class Person {
-private:
+public:
     string name;
     int age;
-}
+};
 
 int main() {
     Person p1;
-    p1.name='John Doe';
-    p1.age=30;
-    
-    
+    p1.name = "John Doe";
+    p1.age = 30;
+
+    cout << "Name: " << p1.name << endl;
+    cout << "Age: " << p1.age << endl;
     return 0;
 }
