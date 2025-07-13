9adaa1d75f57ab37ba2d3f34ba04adf58782b6b7
Natasha-Yadav <natasha100913@gmail.com>
2025-07-13 20:17:24 +0530

    oops

---
+++ oops/getterssetters.cpp
@@ -0,0 +1,17 @@
+int #include <iostream>
+using namespace std;
+
+class Person {
+private:
+    string name;
+    int age;
+}
+
+int main() {
+    Person p1;
+    p1.name='John Doe';
+    p1.age=30;
+    
+    
+    return 0;
+}
