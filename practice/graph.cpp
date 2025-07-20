032e68aedce74e095e68dd7abf90fbc4a4184886
Natasha-Yadav <natasha100913@gmail.com>
2025-07-20 13:59:42 +0530

    changes

--- practice/graph.cpp
+++ practice/graph.cpp
@@ -1,35 +1,14 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+vector<int>dfs(int start,vector<int>adj[]){
+    vector<int>vis(adj->size(), 0);
+    vector<int>result;
+    vis[start] = 1;
+    result.push_back(start);
 
-
-void creategraph(int n,int m){
-    vector<vector<int>> adj(n+1);
-    for(int i=0;i<m;i++){
-        int u,v;
-        cin>>u>>v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-    // Print adjacency list
-    for(int i=1;i<=n;i++){
-        cout << i << ": ";
-        for(int v : adj[i]) {
-            cout << v << " ";
+    for(auto it: adj[start]){
+        if(!vis[it]){
+            vis[it] = 1;
+            dfs(it, adj);
         }
-        cout << endl;
+
     }
 }
-
-int main() {
-    int n,m;
-    cin >> n >> m;
-    creategraph(n,m);
-    return 0;
-}
-
-
-
-
-
-
